175eed9bfe73507cd124f6d2573b51d1b0cd021a
abuaya <74809348+abuaya@users.noreply.github.com>
2021-03-29 12:52:47 +1100

    added axios integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Yo yo waddup 
+        </h1>
+        <Weather city="New York"/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from 'axios';
+
+export default function Weather (props) {
+  function handleResponse (response) {
+    alert(`The weather in New York is ${response.data.main.temp}°C`);
+  }
+  
+let apiKey = `a05b99834a4c4393485b5df92793ef0c`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(handleResponse);
+  return <h2>
+     Hello
+  </h2>;
+}
